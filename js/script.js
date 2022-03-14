a60d3029022b3e268d24c3f6d18249c06a4f82e8
unknown <you@example.com>
2022-03-14 12:25:58 +0600

    finall push

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function () {
     //    sticky header
     $(window).scroll(function () {
-        var Anu = $(this).scrollTop();
-        if (Anu > 100) {
+        var smooth = $(this).scrollTop();
+        if (smooth > 100) {
             $('#navbar').addClass('bg');
         } else {
             $('#navbar').removeClass('bg')
@@ -10,4 +10,19 @@ $(function () {
     })
     //    sticky header
 });
-
+const getToggleBtn = ()=>{
+   const img = document.getElementById("toggle_img");
+   const img2 = document.getElementById("cross_img");
+   img.style.display = "none"
+   img2.style.display = "block"
+    const menu = document.querySelector(".menu_right");
+    menu.style.display = "block";
+}
+const getCrossBtn = ()=>{
+   const img = document.getElementById("toggle_img");
+    const img2 = document.getElementById("cross_img");
+    const menu = document.querySelector(".menu_right");
+   img.style.display = "block"
+    menu.style.display = "none";
+    img2.style.display = "none"
+}
